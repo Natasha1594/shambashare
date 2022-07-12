7e1387f9912df14b9d9081d636a88f160f563bf9
Natasha Wanja <natashawanja@Natashas-MacBook-Pro.local>
2022-07-12 15:31:04 +0300

    firstcommit

---
+++ photos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+
+def add(request):
+    return render(request, 'photos/add.html')
+
+def photo(request):
+    return render(request, 'photos/photo.html')
