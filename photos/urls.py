cb3a11d9c6dab5dceab405ee8127ef83f00285ee
Natasha Wanja <natashawanja@Natashas-MacBook-Pro.local>
2022-07-14 10:26:24 +0300

    update

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/str:pk', views.viewPhoto, name='photo'),
-    path('add/', views.views.addPhoto, name='add'),
+    path('add/', views.addPhoto, name='add'),
 ]
     
 
--- shambashare/settings.py
+++ shambashare/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photos.apps.PhotosConfig',
-    'photos',
+    
 ]
 
 MIDDLEWARE = [
